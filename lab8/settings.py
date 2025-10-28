a1ec949723036dbd912ea178ff887ca98b702dcf
Gabriel Alvarez Rosas <franklinalvarez1307005@gmail.com>
2025-10-28 10:53:12 -0500

    lab5

--- lab8/settings.py
+++ lab8/settings.py
@@ -120,5 +120,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+#MEDIA FILES
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
